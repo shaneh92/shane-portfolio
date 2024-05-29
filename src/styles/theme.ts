8b7af6b77fe345d2bfa5a80f09e90c584e96027d
Shane Hingtgen <shanehingtgen@gmail.com>
2024-05-29 15:57:52 -0500

    more custom theme changes

---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from "@mui/material";
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#41FFC9",
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat",
+    h1: {
+      fontFamily: "Montserrat",
+    },
+    h2: {
+      fontFamily: "Montserrat",
+    },
+    h3: {
+      fontFamily: "Montserrat",
+    },
+    h4: {
+      fontFamily: "Montserrat",
+    },
+    h5: {
+      fontFamily: "Montserrat",
+    },
+    h6: {
+      fontFamily: "Montserrat",
+    },
+    subtitle1: {
+      fontFamily: "Montserrat",
+    },
+    subtitle2: {
+      fontFamily: "Montserrat",
+    },
+    button: {
+      fontFamily: "Montserrat",
+    },
+    body1: {
+      fontFamily: "Montserrat",
+    },
+  },
+});
+export default customTheme;
