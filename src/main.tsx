8bd72919cc79960f4ee8c8ce44db9699ff9680df
Shane Hingtgen <shanehingtgen@gmail.com>
2024-07-06 18:54:05 -0500

    gh pages test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import routes from "./routes/index.tsx";
+// import { ThemeProvider } from "@mui/material/styles";
+// import { theme } from "./styles/theme.ts";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// const router = createBrowserRouter(routes);
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <RouterProvider router={router} />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import routes from "./routes/index.tsx";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./styles/theme.ts";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
-const router = createBrowserRouter(routes);
+const basename = "";
+const router = createHashRouter(routes, { basename });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
