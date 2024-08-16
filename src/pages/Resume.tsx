3b13edfb214caea16c37022d6b3470c9e94d2599
Shane Hingtgen <shanehingtgen@gmail.com>
2024-08-16 16:52:48 -0500

    updating todo list

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -22,12 +22,11 @@ export default function Resume() {
 }
 /**TODO:
  * 1. Fix the font sizes for list items
- * 2. Fix the gap in SkillsResume list items
- * 3. fix education and skills card to be even at bottom
- * 4. Fix hamburger menu to be on the right side
- * 5. Fix skills list on home page to not overlap when screen shrinks
- * 6. Implement new header links and fix the hamburger menu links to work for the entire button rather than just the text
- * 7. Fix the Footer content: needs to move left for even spacing in margins, and the footer needs to be the entire width of container
- * 8. Add Calendly to Schedule a meeting button
- * 9. Make skills into a button that links to their generic webpage, IE: React will link to the React dev webpage
+ * 2. fix education and skills card to be even at bottom
+ * 3. Fix hamburger menu to be on the right side
+ * 4. Fix skills list on home page to not overlap when screen shrinks
+ * 5. Implement new header links and fix the hamburger menu links to work for the entire button rather than just the text
+ * 6. Fix the Footer content: needs to move left for even spacing in margins, and the footer needs to be the entire width of container
+ * 7. Make skills into a button that links to their generic webpage, IE: React will link to the React dev webpage
+ * 8. Add more projects to the projects page
  */
